bbf978857c46adaa59c6ae9d696faff2ff6c0c18
Victor Frazao <luiz_victor_frazao@hotmail.com>
2021-05-22 19:23:06 -0400

    Done move_semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,18 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // Original
+    // let mut vec1 = fill_vec(vec0);
+
+    // Make copy of vec0
+    // let vec0_copy = vec0.to_vec();
+    // let mut vec1 = fill_vec(vec0_copy);
+
+    // Make fill_vec borrow vec0
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,8 +23,14 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+
+// Original
+// fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+//     let mut vec = vec;
+
+// Make fill_vec borrow vec0
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.to_vec();
 
     vec.push(22);
     vec.push(44);
